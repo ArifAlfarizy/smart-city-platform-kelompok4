8993cdbf67ca5e84571639388d61209e56fd3243
Arif Alfarizy <arifalfrzy@gmail.com>
2026-06-15 11:34:40 +0700

    refactor: change responses

--- auth-service/src/utils/tokenHelper.js
+++ auth-service/src/utils/tokenHelper.js
@@ -7,14 +7,6 @@ export const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user.id, email: user.email, role: user.role },
     process.env.JWT_ACCESS_SECRET,
-    { expiresIn: "15m" },
-  );
-};
-
-export const generateRefreshToken = (user) => {
-  return jwt.sign(
-    { id: user.id, email: user.email, role: user.role },
-    process.env.JWT_ACCESS_SECRET,
-    { expiresIn: "7d" },
+    { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN },
   );
 };
