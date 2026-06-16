a5c9f7ef34f19f9fc4577d2a6a9d30b438e9c58b
Arif Alfarizy <arifalfrzy@gmail.com>
2026-06-16 17:11:45 +0700

    feat: oauth service to gateway, token verify for revoking refresh token for users

--- express-gateway/src/index.js
+++ express-gateway/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import verifyToken from "./middleware/verifyToken.js";
 
 const app = express();
 
@@ -8,8 +9,19 @@ app.use((req, res, next) => {
   next();
 });
 
+
+// auth service
 app.use(
-  "/auth",
+  ["/auth", "/oauth"],
+  createProxyMiddleware({
+    target: "http://localhost:3002",
+    changeOrigin: true,
+  }),
+);
+
+app.use(
+  "/oauth/revoke",
+  verifyToken,
   createProxyMiddleware({
     target: "http://localhost:3002",
     changeOrigin: true,
