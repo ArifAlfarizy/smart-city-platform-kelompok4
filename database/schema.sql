2fd0885fef13804cfae87476bc1e32a8abdf855c
cal-dev001 <deevianofcalista@gmail.com>
2026-06-24 18:49:22 +0700

    feat: update citizen service

--- database/schema.sql
+++ database/schema.sql
@@ -59,6 +59,53 @@ CREATE TABLE revoked_tokens (
   revoked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+--table citizen
+CREATE TABLE citizens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    nik VARCHAR(20) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--tabel reports
+CREATE TABLE reports (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    citizen_id INT NOT NULL,
+    road_name VARCHAR(100) NOT NULL,
+    category ENUM(
+        'accident',
+        'broken_vehicle',
+        'fallen_tree',
+        'flood',
+        'road_obstacle',
+        'traffic_light_damage'
+    ) NOT NULL,
+
+    description TEXT NOT NULL,
+    status ENUM(
+        'pending',
+        'process',
+        'completed'
+    ) DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (citizen_id) REFERENCES citizens(id)
+        ON DELETE CASCADE
+);
+
+--table notification
+CREATE TABLE notifications (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    citizen_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    message TEXT NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (citizen_id) REFERENCES citizens(id)
+        ON DELETE CASCADE
+);
+
 -- Tabel traffic_data
 CREATE TABLE traffic_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
